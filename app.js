1572f67c2bf1a3507d2f2c98f87bb76187b4bc7a
Goksel Ozkazanc <goksel_ozkazanc_08@hotmail.com>
2021-01-06 09:54:12 +0100

    completed for now

---
+++ app.js
@@ -0,0 +1,68 @@
+$(window).on("load", function () {
+    $("body").on("touchstart", function(){})
+
+    $(".banner-button").on("click", function () {  //these buttons carry us to pokemons-page
+        $(".banner").css("display", "none")
+        $(".pokemonsPage").css("display", "flex")
+    })
+
+    $(".poke-img").on("mouseover", function () {            //mouseover on pokemons and some styles add
+        $(this).css({                                       //select only the element which is mouseovered, not select all the elements which has .poke-img class 
+            "filter": "blur(5px)",
+            "animation": "pokeImg-anim 1s"
+        })
+        $(this).siblings(".poke-name").css("opacity", "1")  //select this' elements sibling which has class of "poke-name"
+    })
+    $(".poke-img").on("mouseout", function () {
+        $(this).css({
+            "filter": "blur(0px)",
+            "animation": "none"
+        })
+        $(this).siblings(".poke-name").css("opacity", "0")
+    })
+
+    $(".poke-img").on("click", function () {            //
+        $(".poke-audio").trigger("pause")               //
+        $(this).siblings(".poke-audio").trigger("play") //
+                                                        //
+        $(".poke-img").css({                            //
+            "filter": "blur(0px)",                      //
+            "animation": "none"                         //
+        })                                              //  This block of code is for touch-screens when clicking 
+        $(this).css({                                   //
+            "filter": "blur(5px)",                      //
+            "animation": "anim 1s"                      //////
+        })                                                  //
+                                                            //
+        $(".poke-name").css("opacity", "0")                 //
+        $(this).siblings(".poke-name").css("opacity", "1")  //
+    })
+
+    // $(".poke-img").each(function () {            // This is the alternative code of previous block of code
+    //     $(this).on("click", function () {        // But in this code, current pokemon's audio does not stop when clicking others
+    //         $(this).css({                        // Therefore, multiple pokemons' audios can play at the same time
+    //             "filter": "blur(0px)",
+    //             "animation": "none"
+    //         })
+    //         $(this).siblings(".poke-audio").trigger("play")
+    //         $(this).siblings(".poke-name").css("opacity", "1")
+    //     })
+    // })
+
+    var pokeImg = $(".poke-img").length;                    
+    for (var i = 1; i <= pokeImg; i++) {   //instead of adding every pokemons'image manuelly, this loop does it automatically
+        $(`.poke-img-${i}`).css({
+            "background": `url(images/pokemon-${i}.png) no-repeat center center / contain`
+        })
+    }
+
+    $(".return-banner").on("click", function () { //this button(X) carry us to banner page
+        $(".banner").css("display", "flex")
+        $(".pokemonsPage").css("display", "none")
+        $(".poke-audio").trigger("pause")      
+    })
+
+    $(".poke-img").on("dblclick", function () {     //audios stop when double clicking on pokemons
+        $(".poke-audio").trigger("pause")
+    })
+})
